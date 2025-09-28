088403d07b3a518a43d1e96e5ca7aed75e76f8f2
thabang165 <atlemola@gmail.com>
2025-09-28 19:04:37 +0200

    fix bug

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/<int:pk>', BookUpdateView.as_view(), name='book-update'),  # moved 'update' before pk
+    path('books/delete/<int:pk>', BookDeleteView.as_view(), name='book-delete'),  # moved 'delete' before pk
 ]
 
