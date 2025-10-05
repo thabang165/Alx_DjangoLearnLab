6bd0e3ea4e4ac111bba203a4ce203e012654e4e1
thabang165 <atlemola@gmail.com>
2025-10-05 02:24:25 +0200

    first commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+# Registration
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Login
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+# Logout
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# Profile
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        user = request.user
+        email = request.POST.get('email')
+        if email:
+            user.email = email
+            user.save()
+    return render(request, 'blog/profile.html', {'user': request.user})
+
